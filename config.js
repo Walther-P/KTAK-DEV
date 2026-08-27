a5b445aa0ad886f47bcb6579a559da468ac33fb4
Walther-P <ht500119es@gmail.com>
2026-08-27 22:46:16 +0800

    Add Google Maps key to DEV
    Add Google Maps key to DEV

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 window.KTAK_CONFIG = {
   SUPABASE_URL: "https://vrkojmzgvlrrvhplirna.supabase.co",
   SUPABASE_PUBLISHABLE_KEY: "sb_publishable_stD7lzX7_K0mOwY2HJR1gw_52OPcTOQ",
-  TURNSTILE_SITE_KEY: "0x4AAAAAAEcrMapslwAv_rVw"
+  TURNSTILE_SITE_KEY: "0x4AAAAAAEcrMapslwAv_rVw",
+  GOOGLE_MAPS_API_KEY: "AIzaSyDX48FoFkAsVar0l7Q07OcfRnVGBfpxx7s"
 };
