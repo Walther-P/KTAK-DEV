1db53d8145ddc1e22e5424aaec85b624e4966aec
Walther-P <ht500119es@gmail.com>
2026-08-28 16:53:25 +0800

    Update DEV to V2.1 DEV6-A room lifecycle
    Update DEV to V2.1 DEV6-A room lifecycle

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="ktak-v21-dev51-retirement";
+const VERSION="ktak-v21-dev6a-retirement";
 self.addEventListener("install",e=>self.skipWaiting());
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith("ktak-")).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(fetch(e.request,{cache:"no-store"}).catch(()=>fetch(e.request)))});
